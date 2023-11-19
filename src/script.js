6cdecf32bc27c10eaa914d5b76f2e871f73b084a
Matheus Rodrigues <rmatheus440@gmail.com>
2023-11-19 18:26:33 -0300

    Update

--- src/script.js
+++ src/script.js
@@ -1,23 +1,53 @@
-//função de mudar imagem pelo id e pela url
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+let currentPokemonIndex = 1;
+
+function loadPokemon(index) {
+  fetch(baseUrl + index)
+    .then(response => response.json())
+    .then(data => {
+      changeImage('img_sprite_front_default', data.sprites.front_default);
+      changeText('name', data.name);
+    })
+    .catch(error => {
+      console.error('Error fetching data: ', error);
+      alert('Houve um erro ao carregar os dados do Pokémon.');
+    });
+}
+
+function previousPokemon() {
+  if (currentPokemonIndex === 1) {
+    fetch(baseUrl)
+      .then(response => response.json())
+      .then(data => {
+        currentPokemonIndex = data.count;
+        loadPokemon(currentPokemonIndex);
+      });
+  } else {
+    currentPokemonIndex--;
+    loadPokemon(currentPokemonIndex);
+  }
+}
+
+function nextPokemon() {
+  fetch(baseUrl)
+    .then(response => response.json())
+    .then(data => {
+      if (currentPokemonIndex >= data.count) {
+        currentPokemonIndex = 1;
+      } else {
+        currentPokemonIndex++;
+      }
+      loadPokemon(currentPokemonIndex);
+    });
+}
+
 function changeImage(id, url) {
   document.getElementById(id).src = url;
 }
-//função de mudar texto pelo id e pelo texto
+
 function changeText(id, text) {
   document.getElementById(id).innerText = text;
 }
 
-// Daqui para baixo voce ira escrever
-// o código para resolver o desafio
-
-function previousPokemon() {
-  alert("Pokemon Anterior");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Anterior");
-}
-
-function nextPokemon() {
-  alert("Pokemon Seguinte");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Seguinte");
-}
+loadPokemon(currentPokemonIndex);
